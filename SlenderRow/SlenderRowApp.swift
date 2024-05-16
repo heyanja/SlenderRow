5b72ef26152c79c57571f7d4e1350d3087c3e0c4
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-16 11:03:36 -0400

    Initial Commit

---
+++ SlenderRow/SlenderRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlenderRowApp.swift
+//  SlenderRow
+//
+//  Created by Anna Fadieieva on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct SlenderRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
