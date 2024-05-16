9267a5d65d41bba4650e45100fae96909ef5e553
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-16 12:21:57 -0400

    6

--- SlenderRow/ContentView.swift
+++ SlenderRow/ContentView.swift
@@ -7,20 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct DiagonalStack: Layout {
+    func sizeThatFits(
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) -> CGSize {
+        subviews.reduce(CGSize.zero) { result, subview in
+            let size = subview.sizeThatFits(.unspecified)
+            return CGSize(
+                width: proposal.width ?? 0,
+                height: result.height + size.height)
         }
-        .padding()
+    }
+    
+    func placeSubviews(
+        in bounds: CGRect,
+        proposal: ProposedViewSize,
+        subviews: Subviews,
+        cache: inout ()
+    ) {
+        var point = bounds.origin
+        var xPosition = bounds.maxX - subviews[0].dimensions(in: .unspecified).width
+        let step = abs(bounds.minX - xPosition) / Double(subviews.count - 1)
+        
+        for subview in subviews.reversed() {
+            point.x = xPosition
+            subview.place(at: point, anchor: .zero, proposal: .unspecified)
+            xPosition -= step
+            point.y += subview.dimensions(in: .unspecified).height
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @State private var isDiagonal = false
+    @State private var numberOfSquares: CGFloat = 7
+    let stackSpacing: CGFloat = 10
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let layout = isDiagonal ? AnyLayout(DiagonalStack()) : AnyLayout(HStackLayout(spacing: stackSpacing))
+            let squareSize: CGFloat = isDiagonal ?
+            geometry.size.height / numberOfSquares :
+            (geometry.size.width - stackSpacing * (numberOfSquares - 1)) / numberOfSquares
+            HStack {
+                Button(action: {
+                    if numberOfSquares > 1 {
+                        numberOfSquares -= 1
+                    }
+                }) {
+//                    Image(systemName: "minus.circle.fill")
+//                        .font(.largeTitle)
+                }
+                Button(action: {
+                    numberOfSquares += 1
+                }) {
+//                    Image(systemName: "plus.circle.fill")
+//                        .font(.largeTitle)
+                }
+            }
+            
+            .padding()
+            .frame(width: geometry.size.width)
+            layout {
+                ForEach(0..<Int(self.numberOfSquares), id: \.self) { index in
+                    Rectangle()
+                        .fill(Color.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .frame(
+                            width: squareSize,
+                            height: squareSize)
+                        .animation(.smooth, value: isDiagonal)
+                        .animation(.bouncy, value: numberOfSquares)
+                }
+            }
+            .onTapGesture {
+                withAnimation {
+                    self.isDiagonal.toggle()
+                }
+                
+            }
+            .frame(height: geometry.size.height)
+        }
+        
+    }
+}
+extension Animation {
+    static var smooth: Animation {
+        .interpolatingSpring(stiffness: 100, damping: 10)
+    }
+    static var bouncy: Animation {
+        .interpolatingSpring(stiffness: 100, damping: 5)
     }
 }
